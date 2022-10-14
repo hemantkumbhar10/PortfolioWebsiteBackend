36714d413135bbe7b4346317cf5ff7f7cd729ec8
Hemant Kumbhar <hemantkumbhar010@gmail.com>
2022-10-14 05:57:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,34 +2,20 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-
-
-// const corsOptions={
-//     origin: "https://6348a3adc723983000192f9f--hemantkumbhar10.netlify.app/",
-//     credentials:true, 
-// }
-
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
+
+const corsOptions={
+    origin: "https://6348a3adc723983000192f9f--hemantkumbhar10.netlify.app/",
+    credentials:true, 
+}
 
 
 
-// app.use(cors(corsOptions));
+
+
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
